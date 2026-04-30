c90759b0331e5aa5533f358c5c647bc99f12309e
B-ronC <b0ron0cai@gmail.com>
2026-04-29 19:27:39 -0700

    update embedding model

--- get_embedding_function.py
+++ get_embedding_function.py
@@ -3,6 +3,6 @@ from langchain_ollama import OllamaEmbeddings
 # embedding function
 def get_embedding_function():
     embeddings = OllamaEmbeddings(
-        model="qwen3-embedding",
+        model="nomic-embed-text",
     )
     return embeddings
